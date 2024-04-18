8e18f69d1d13ab5e2495364c24b887cf687b3c53
JAYANTH J R <93864425+Jayanth173@users.noreply.github.com>
2024-04-18 21:29:34 +0530

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
+import chatlogo from './assets/chatgpt.svg';
+import addbtn from './assets/add-30.png'
+import msgIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='sideBar'>
+        <div className='upperside'>
+          <div className='uppersidetop'>
+            <img src={chatlogo} alt='Logo' className='logo'/>
+            <span className='brand'>
+              We Chat
+            </span>
+            </div>
+            <button className='midbtn'>
+              <img src={addbtn} alt='new chat' className='addbtn'/>New Chat
+            </button>
+            <div className='uppersidebottom'>
+              <button className='query'>
+                <img src={msgIcon} alt='Query'/>What is Programming
+              </button>
+              <br/>
+              <button className='query'>
+                <img src={msgIcon} alt='Query'/>How to use Api
+              </button>
+            </div>
+          </div>
+        <div className='lowerside'>
+                <div className='listitems'>
+                  <img src={home} alt='Home' className='listitemimg'/>Home
+                </div>
+                <div className='listitems'>
+                  <img src={saved} alt='Saved' className='listitemimg'/>Saved
+                </div>
+                <div className='listitems'>
+                  <img src={rocket} alt='Rocket' className='listitemimg'/>Upgrade Pro
+                </div>
+        </div>
+      </div>
+      </div>
+
   );
 }
 
